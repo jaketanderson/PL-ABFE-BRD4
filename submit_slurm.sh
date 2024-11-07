a91d57df6969f01371ea1f3705acd61a57fba34d
Jake Anderson <jta002@ucsd.edu>
2024-11-07 12:53:32 -0800

    Updated submit_slurm.sh for many GPUs on tscc

--- submit_slurm.sh
+++ submit_slurm.sh
@@ -2,13 +2,13 @@
 
 #SBATCH --job-name="PL-ABFE-BRD4"
 #SBATCH --partition=rtx3090
-#SBATCH --propagate=NONE
-#SBATCH --cpus-per-gpu=1
-#SBATCH --ntasks=4
-#SBATCH --mem=32G
-#SBATCH --time=3-00:00:00
+#SBATCH --ntasks=8
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4G
+#SBATCH --time=0-00:10:00
 #SBATCH -q hca-csd765
-#SBATCH --gpus 16
+#SBATCH --gpus=8
+#SBATCH --nodes=1-8
 #SBATCH -A ddp325
 
 Folder=$(pwd)
@@ -26,7 +26,29 @@ export JAX_ENABLE_X64=True
 cd /tscc/nfs/home/jta002/workspace/PL-ABFE/PL-ABFE-BRD4
 
 echo "Starting job at $(date)"
-ray start --head --num-gpus=16 --port=8835
-srun ray start --address=$(hostname):8835
-python main.py
-echo "Finished job at $(date)"
+
+HEAD_NODE=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)  # Get the first node as the head
+HEAD_PORT=8836  # Port for Ray head node
+
+if [[ "$SLURM_NODEID" == "0" ]]; then
+    # Start Ray head node
+    echo "Starting Ray head on $HEAD_NODE"
+    ray start --head --port=$HEAD_PORT
+
+    # Check every 10 seconds for connected workers
+    for i in {1..18}; do
+        sleep 10
+        worker_count=$(ray status | grep -c "node")
+        if [[ $worker_count -ge 8 ]]; then
+            break
+        fi
+    done
+
+    python main.py
+
+    ray stop
+else
+    # Start Ray worker nodes
+    echo "Starting Ray worker on $SLURM_NODEID, connecting to head node at $HEAD_NODE:$HEAD_PORT"
+    ray start --address="$HEAD_NODE:$HEAD_PORT"
+fi
